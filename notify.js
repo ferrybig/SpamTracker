bc9d8c09b489533ab152a650afb90fcf83d2ac84
Normal Human <normalhuman@users.noreply.github.com>
2015-07-05 01:17:13 -0400

    unified the handling of notifications

--- notify.js
+++ notify.js
@@ -8,12 +8,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
     opt.title = msg.title;
     opt.message = msg.message;
     opt.iconUrl = 'icon128.png';
-    if (msg.type == 'question') {
-      opt.buttons = [{title: 'False Alarm'}];
-    }
-    if (msg.type == 'chat') {
-      opt.buttons = [{title: 'Go To Room'}];
-    }
+    opt.buttons = [{title: 'False Alarm'}, {title: 'Go To Room'}];
     chrome.notifications.create(msg.id, opt, callbackNotification);
   }
 });
@@ -21,12 +16,9 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
 
 chrome.notifications.onClicked.addListener(function(id) {
   var t = id.split('-');
-  if (/^chat/.test(t[0]) && t[1] != 'na') {
-    chrome.windows.create({url: 'http://'+t[1]+'/q/'+t[2]});
-  }
-  if (!/^chat/.test(t[0]))  {
-    chrome.windows.create({url: 'http://'+t[0]+'/q/'+t[1]});
-    chrome.tabs.query({url: '*://stackexchange.com/search?tab=realtime'}, function(tabs) {
+  if (t[1] != 'na') {
+    chrome.windows.create({url: 'http://'+t[1]+'/q/'+t[2], width: 750});
+    chrome.tabs.query({url: '*://'+t[0]+'*'}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {dismiss: id});
     });
   }
@@ -35,14 +27,14 @@ chrome.notifications.onClicked.addListener(function(id) {
 
 
 function dismissRealtime(id) {
-  chrome.tabs.query({url: '*://stackexchange.com/search?tab=realtime'}, function(tabs) {
+  chrome.tabs.query({url: '*://'+id.split('-')[0]+'*'}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {dismiss: id});
   });
   chrome.notifications.clear(id, callbackNotificationCleared);
 }
 
 chrome.notifications.onButtonClicked.addListener(function(id,buttonIndex) {
-  if (/^chat/.test(id)) {
+  if (buttonIndex == 1) {
     chrome.tabs.query({url: '*://'+id.split('-')[0]+'*'}, function(tabs) {
       chrome.tabs.update(tabs[0].id, {active: true});
     });
